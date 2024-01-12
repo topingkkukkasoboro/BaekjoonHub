cfaaa1e3a8c492240357e0c585182e99a5b22cf1
topingkkukkasoboro <155594382+topingkkukkasoboro@users.noreply.github.com>
2024-01-12 16:50:55 +0900

    [level Lv.0] Title: n의 배수, Time: 0.01 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int n) {
+		 int answer = 0;
+		 int result = 1;
+	        
+	        if(num%n==0) {
+	        	return result;
+	        }else
+	        	return answer;
+	    
+	    }
+	}
