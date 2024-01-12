e1b7410b6ee7c565ba35f77fa9958842f3168521
topingkkukkasoboro <155594382+topingkkukkasoboro@users.noreply.github.com>
2024-01-12 16:13:02 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 12880 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b); //a+b를 출력해라
+        System.out.println(a - b); //a-b를 출력해라
+        System.out.println(a * b); //a*b를 출력해라
+        System.out.println(a / b); //a/b를 출력해라
+        System.out.println(a % b); //a%b를 출력해라
+    }
+}
