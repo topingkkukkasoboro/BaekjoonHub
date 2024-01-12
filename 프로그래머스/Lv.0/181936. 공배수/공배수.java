678223f00b2a99ea235b7fdfa5a77a5afb523184
topingkkukkasoboro <155594382+topingkkukkasoboro@users.noreply.github.com>
2024-01-12 16:51:07 +0900

    [level Lv.0] Title: 공배수, Time: 0.01 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181936. 공배수/공배수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+int answer = 1;
+	        int result = 0;
+	        
+	        if(number%n==0 && number%m == 0) {
+	        return answer;	
+	        }else {
+	        	return result;
+	        }
+	        
+	        
+	        
+	        
+	        
+	   
+	    }
+	}
